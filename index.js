d4fca01bce8fa3ef278d82643c1b85c4655b91fe
Harshit Srivastava <harshitsri1625@gmail.com>
2024-06-30 06:28:37 +0530

    third commit

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 //to access public files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Base Route
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
+
+app.get("/api/", function (req, res) {
+  const date = new Date();
+  res.json({ unix : date.getTime() , utc : date.toUTCString() });
+});
+
+
 
 app.get("/api/:dateInput", function (req, res) {
-  const {dateInput} = req.params;
-  let date
+  const { dateInput } = req.params;
+  let date;
 
-  if(dateInput > 10){
-     if(dateInput.includes(" ")){
-      date = new Date.parse(dateInput)
-     }else{
-      date = parseInt(dateInput)
-     }
-  }
-  else{
-    date = dateInput
+  if (dateInput > 10) {
+    if (dateInput.includes(" ")) {
+      date = new Date.parse(dateInput);
+    } else {
+      date = parseInt(dateInput);
+    }
+  } else {
+    date = dateInput;
   }
 
   const finalDate = new Date(date);
 
-  const timestamp ={
-    unix : finalDate.getTime(),
-    utc: finalDate.toUTCString()
-  }
+  const timestamp = {
+    unix: finalDate.getTime(),
+    utc: finalDate.toUTCString(),
+  };
 
-  if(!finalDate.getTime()) return res.status(404).json({ error : "Invalid date"})
+  if (!finalDate.getTime())
+    return res.status(404).json({ error: "Invalid date" });
 
   res.status(200).json(timestamp);
 });
 
-
-
 var listener = app.listen(3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
